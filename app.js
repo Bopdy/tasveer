a4c5f677562e6a01c603e96b37b5b2cd2b1a9063
Pankaj Sharma <pankaj@overallsite.com>
2020-08-22 21:16:31 +0530

    updated home

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const express = require("express");
+const express = require('express')
+const ejs = require('ejs')
+const bodyParser = require('body-parser')
+const app = express()
+const path = require('path')
 
-const app = express();
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get("", (req, res) => {
-  res.send("Welcome to Tasveer!");
-});
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 app.listen(3000, () => {
-  console.log("Server has started on port 3000");
-});
+  console.log('Server has started on port 3000')
+})
