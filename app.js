e941a8affc2b211ab3e242197412c31593655539
Pankaj Sharma <pankaj@overallsite.com>
2020-08-22 06:36:51 +0530

    welcome

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("Hello Guys! Welcome to Tasveer!");
+  res.send("Welcome to Tasveer!");
 });
 
 app.listen(3000, () => {
